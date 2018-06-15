c544a538e8dc19c278d7aa4d74d99ff863b6e8bc
Btrumps <btrumpsdev@gmail.com>
2018-06-15 08:36:37 -0400

    level text changed to numbers while we are adding/editing levels

--- js/GameUI.js
+++ js/GameUI.js
@@ -3,18 +3,22 @@ const TEXT_LEVEL_START_Y = 25;
 
 function showLevelText() {
 	if (currentLevel == 1) {
-		colorText('Baby Jumps', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+
 	} else if (currentLevel == 2) {
-		colorText('You look dashing, my friend', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+		colorText('1-1', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
 	} else if (currentLevel == 3) {
-		colorText('Ah, refreshing...', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+		colorText('1-2', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
 	} else if (currentLevel == 4) {
-		colorText('Going up', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+		colorText('1-3', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
 	} else if (currentLevel == 5) {
-		colorText('Quick fingers', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+		colorText('1-4', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
 	} else if (currentLevel == 6) {
-		colorText('Bit of a refresher', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+		colorText('1-5', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
 	} else if (currentLevel == 7) {
-		colorText('Sadistic', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+		colorText('1-6', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+	} else if (currentLevel == 8) {
+		colorText('1-7', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
+	} else if (currentLevel == 9) {
+		colorText('1-8', TEXT_LEVEL_START_X, TEXT_LEVEL_START_Y, 'white', FONT_LEVEL_NAME);
 	}
 }
